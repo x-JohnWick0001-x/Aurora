bd74644c2edaa117c127a49975a6a8f4ba0c8047
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-12-28 20:15:06 -0500

    Create Config class and status command

---
+++ src/cogs/config.py
@@ -0,0 +1,25 @@
+import json
+from discord.ext import commands
+
+STATUS_OPTIONS = ("idle", "invisible", "online", "dnd")
+
+
+class Config(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def status(self, ctx, *, status):
+        if status.lower() not in STATUS_OPTIONS:
+            await ctx.message.edit(
+                "Valid options: "
+                + ", ".join(option.capitalize() for option in STATUS_OPTIONS)
+            )
+
+        config = {"status": status}
+        with open("config.json") as file:
+            json.dump(config, file)
+
+
+def setup(client):
+    client.add_cog(Config(client))
