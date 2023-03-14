116a79ed7fe8d0228f80da457a50b08e523ce5ac
JohnWick000101 <127307440+JohnWick000101@users.noreply.github.com>
2023-03-13 22:31:39 -0700

    remove edit support cuz cringe

--- src/main.py
+++ src/main.py
@@ -38,27 +38,6 @@ async def on_ready():
         except Exception as e:
             print(f"Error when loading {cog}\n{e}")
 
-
-@client.event
-async def on_raw_message_edit(payload):
-    author = payload.data.get("author")
-
-    if not author or author["id"] != str(client.user.id):
-        return
-
-    if payload.cached_message:  # message cached
-        message = payload.cached_message
-
-    else:  # fetch message
-        channel = client.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
-
-    if not message.content.startswith(client.command_prefix):
-        return
-
-    await client.process_commands(message)
-
-
 try:
     stdout = sys.stdout
     f = open("log.txt", "w")
